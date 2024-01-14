4fd234d63558555bd9ae2297ae291aebc1957624
Rakteem Mullick <rm1011.coding@gmail.com>
2024-01-14 15:11:13 +0530

    Update api/index.js

--- api/index.js
+++ api/index.js
@@ -1,62 +1,63 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import usersRoute from "./routes/users.js"
-import hotelRoute from "./routes/hotels.js"
-import roomRoute from "./routes/rooms.js"
-import authRoute from "./routes/auth.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import usersRoute from "./routes/users.js";
+import hotelRoute from "./routes/hotels.js";
+import roomRoute from "./routes/rooms.js";
+import authRoute from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express()
-mongoose.set('strictQuery', false);
-dotenv.config()
+const app = express();
+mongoose.set("strictQuery", false);
+dotenv.config();
 
-
-const MONGO = process.env.MONGO
+const MONGO = process.env.MONGO;
 
 const connect = async () => {
-    try{
-        mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
+  try {
+    mongoose.connect(MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log("connected to mongodb")
-    }catch(error){
-        throw error;
-    }
+    console.log("connected to mongodb");
+  } catch (error) {
+    throw error;
+  }
 };
 
 mongoose.connection.on("disconnected", () => {
-    console.log("mongoDB disconnected!")
-})
-
-mongoose.connection.on("connected", () => {
-    console.log("mongoDB connected!")
-})
-
-//middlewares
-app.use(cors())
-app.use(cookieParser())
-app.use(express.json())
-
-app.use("/api/auth", authRoute)
-app.use("/api/users", usersRoute)
-app.use("/api/hotels", hotelRoute)
-app.use("/api/rooms", roomRoute)
-
-app.use((err, req, res, next) => {
-    
-    const errorStatus = err.status || 500;
-    const errorMessage = err.message || "Something went wrong. Please try again!"
-
-    return res.status(errorStatus).json({
-        success: false,
-        status: errorStatus,
-        message: errorMessage,
-        stack: err.stack
-    });
+  console.log("mongoDB disconnected!");
 });
 
-app.listen(8800, ()=> {
-    connect()
-    console.log("Connected to backend.")
-})
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+//middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelRoute);
+app.use("/api/rooms", roomRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong. Please try again!";
+
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(process.env.port || 8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
